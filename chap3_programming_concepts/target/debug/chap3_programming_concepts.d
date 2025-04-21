475f12f90dbe6a2cc0611d5d96528c26a4b441a0
unknown <seanjamison@pm.me>
2025-04-20 20:01:09 -0700

    2 ways of desctructuring tuples

---
+++ chap3_programming_concepts/target/debug/chap3_programming_concepts.d
@@ -0,0 +1,1 @@
+C:\Users\Sean\Documents\RustProjects\chap3_programming_concepts\target\debug\chap3_programming_concepts.exe: C:\Users\Sean\Documents\RustProjects\chap3_programming_concepts\src\main.rs
---
+++ chap3_programming_concepts/target/debug/deps/chap3_programming_concepts.d
@@ -0,0 +1,5 @@
+C:\Users\Sean\Documents\RustProjects\chap3_programming_concepts\target\debug\deps\chap3_programming_concepts.exe: src\main.rs
+
+C:\Users\Sean\Documents\RustProjects\chap3_programming_concepts\target\debug\deps\chap3_programming_concepts.d: src\main.rs
+
+src\main.rs:
