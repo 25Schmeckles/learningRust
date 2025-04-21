7c968cec8ff1e9aedf8d4bd8949308b1d0694545
unknown <seanjamison@pm.me>
2025-04-20 20:14:01 -0700

    vector,array, and tuple examples

--- chap3_programming_concepts/src/main.rs
+++ chap3_programming_concepts/src/main.rs
@@ -7,4 +7,23 @@ fn main() {
         let five_hundred = x.0;
         let six_point_four = x.1;
         let one = x.2;
+
+        //array accessing
+        let a = [1, 2, 3, 4, 5];
+
+        let first = a[0];
+        let second = a[1];
+
+        //vector accessing
+        let mut v = Vec::new();
+
+        v.push(5);
+        v.push(6);
+        v.push(7);
+        v.push(8);
+
+        let v = vec![1, 2, 3, 4, 5];
+
+        println!("The value at 0 is: {}",v[0]);
+
 }
