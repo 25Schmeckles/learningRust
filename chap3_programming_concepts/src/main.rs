031a9e456963dfe3c42de3ee2af48533239e38b3
unknown <seanjamison@pm.me>
2025-04-22 17:20:58 -0700

    loop types, labeled loops, continue/break, and ranges examples

--- chap3_programming_concepts/src/main.rs
+++ chap3_programming_concepts/src/main.rs
@@ -28,6 +28,66 @@ fn main() {
         let x = five();
 
         println!("The value of x is: {x}");
+
+        let condition = true;
+        let number = if condition { 5 } else { 6 }; //ensure the types of each of these matches!
+
+        if number % 4 == 0 {
+            println!("number is divisible by 4");
+        } else if number % 3 == 0 {
+            println!("number is divisible by 3");
+        } else if number % 2 == 0 {
+            println!("number is divisible by 2");
+        } else {
+            println!("number is not divisible by 4, 3, or 2");
+        }
+
+        //loop types are: loop,while,for
+        //break statements always return a value, continue does not but a break needs a ;
+        //you can also label loops and specifically break/continue to that loop
+        let mut count = 0;
+        'counting_up: loop { //labeled loop
+            println!("count = {count}");
+            let mut remaining = 10;
+    
+            loop {
+                println!("remaining = {remaining}");
+                if remaining == 9 {
+                    break;
+                }
+                if count == 2 {
+                    break 'counting_up;
+                }
+                remaining -= 1;
+            }
+    
+            count += 1;
+        }
+        println!("End count = {count}");
+
+        //while example
+        let a = [10, 20, 30, 40, 50];
+        let mut index = 0;
+    
+        while index < 5 {
+            println!("the value is: {}", a[index]);
+    
+            index += 1;
+        }
+
+        //for example, safest and preferred loop type in rust
+        let a = [10, 20, 30, 40, 50];
+
+        for element in a {
+            println!("the value is: {element}");
+        }
+        //another for loop with a range, range starts on 1 and ends on 3
+        for number in (1..4).rev() {
+                println!("{number}!");
+            }
+            println!("LIFTOFF!!!");
+
+
 }
 //shows implicit return of an expression (no ;), type must be defined for return, 
 // statements end with ; and do not return
