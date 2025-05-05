4bc437be6fed3b66979a6bbac43dd1765e9155ee
unknown <seanjamison@pm.me>
2025-05-04 18:17:37 -0700

    began romanToInteger, mapped from string to vec of integers, need to use a match for less than greater than then add and return

--- leetCodeProblems/13_romanToInteger/romanToInteger/src/main.rs
+++ leetCodeProblems/13_romanToInteger/romanToInteger/src/main.rs
@@ -1,8 +1,26 @@
+use std::collections::HashMap;
+
+
 fn main() {
-    let s = String::from("V");
+    let s = String::from("VIVXCDM");
     roman_to_int(s);
 }
 pub fn roman_to_int(s: String) -> i32 {
-    println!("{}",s);
+    let numerals = HashMap::from([('I',1),('V',5),('X',10),('L',50),('C',100),('D',500),('M',1000)]);
+    let output: Vec<i32> = s
+        .chars()
+        .map(|ch| numerals[&ch])
+        .collect();
+    
+    
+    let as_string: String = output
+        .iter()
+        .map(|n| n.to_string())
+        .collect::<String>();
+    println!("{}",as_string);
+    
+
+
+
     return 5
 }
