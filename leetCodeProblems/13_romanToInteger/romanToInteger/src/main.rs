35e1f92bef390c4102d6da18dbb3590511bcd3c2
unknown <seanjamison@pm.me>
2025-05-04 19:48:02 -0700

    completed with 3 given testcases, accepted by leetcode with 0ms runtime with linear time

--- leetCodeProblems/13_romanToInteger/romanToInteger/src/main.rs
+++ leetCodeProblems/13_romanToInteger/romanToInteger/src/main.rs
@@ -1,26 +1,26 @@
 use std::collections::HashMap;
 
-
 fn main() {
-    let s = String::from("VIVXCDM");
-    roman_to_int(s);
+    //let s = String::from("iXX");
+    let answer = roman_to_int("III".to_uppercase());
+    println!("{}",answer);
+    let answer = roman_to_int("LVIII".to_uppercase());
+    println!("{}",answer);
+    let answer = roman_to_int("MCMXCIV".to_uppercase());
+    println!("{}",answer);
 }
 pub fn roman_to_int(s: String) -> i32 {
     let numerals = HashMap::from([('I',1),('V',5),('X',10),('L',50),('C',100),('D',500),('M',1000)]);
-    let output: Vec<i32> = s
+    //convert from numeral to vector of numbers
+    let mut numbers: Vec<i32> = s
         .chars()
         .map(|ch| numerals[&ch])
         .collect();
-    
-    
-    let as_string: String = output
-        .iter()
-        .map(|n| n.to_string())
-        .collect::<String>();
-    println!("{}",as_string);
-    
-
-
-
-    return 5
+    //if the next value is bigger than previous flip the sign
+        for i in 0..numbers.len() - 1 {
+            if numbers[i + 1] > numbers[i] {
+                numbers[i] = -numbers[i];
+            }
+        }
+    return numbers.iter().sum();
 }
