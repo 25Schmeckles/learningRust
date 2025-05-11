212e9648cf1b21ef917edcdabc7394648cc25ac9
unknown <seanjamison@pm.me>
2025-05-10 17:31:06 -0700

    double loop version of this problem

--- leetCodeProblems/1_twoSum/twoSum/src/main.rs
+++ leetCodeProblems/1_twoSum/twoSum/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    two_sum(vec![1,2,3],3);
+    let value = two_sum(vec![0,2,4],6);
+    println!("{}{}",value[0],value[1]);
 }
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        return vec![nums[0]+target];
+    for (outer_index,outer_value) in nums.iter().enumerate() {
+        for (inner_index,inner_value) in nums[1+outer_index..].iter().enumerate() {
+            if outer_value + inner_value == target { return vec![outer_index as i32,1+inner_index as i32 + outer_index as i32] }
+        }
+    }
+    return vec![-1,-1];
 }
