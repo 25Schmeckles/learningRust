3152caf3f4e785ec253f36bdff71a0e1bf45f927
unknown <seanjamison@pm.me>
2025-05-12 17:30:34 -0700

    setup functions and figured out accessing elements

--- leetCodeProblems/3_addTwoNumbers/addTwoNumbers/src/main.rs
+++ leetCodeProblems/3_addTwoNumbers/addTwoNumbers/src/main.rs
@@ -22,14 +22,24 @@ fn main() {
         val: 2,
         next: None,
     });
-    add_two_numbers(Some(boxed_list_node1),Some(boxed_list_node2));
-    println!("Hello, world!");
+    let return_added_numbers = add_two_numbers(Some(boxed_list_node1),Some(boxed_list_node2));
+    if let Some(return_unwrap) = &return_added_numbers {
+        println!("{}",&return_unwrap.val);
+    }
 }
 
 pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+   let mut added_number = 0;
+   let next_node = None;
+    if let Some(l1_unwrap) = &l1 {
+        added_number = added_number + &l1_unwrap.val;
+    }
+    if let Some(l2_unwrap) = &l2 {
+        added_number = added_number + &l2_unwrap.val;
+    }
     let boxed_list_node = Box::new(ListNode {
-        val: 3,
-        next: None,
+        val: added_number,
+        next: next_node,
     });
     return Some(boxed_list_node);
 }
