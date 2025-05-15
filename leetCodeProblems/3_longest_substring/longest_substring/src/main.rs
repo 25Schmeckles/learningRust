fe7581dedfa4c11f2ab6877ef97396067878f94a
unknown <seanjamison@pm.me>
2025-05-15 16:22:24 -0700

    created function that finds longest string without 2 letters in a row not without any substring repetition

--- leetCodeProblems/3_longest_substring/longest_substring/src/main.rs
+++ leetCodeProblems/3_longest_substring/longest_substring/src/main.rs
@@ -1,6 +1,25 @@
 fn main() {
-    println!("{}",length_of_longest_substring("Hello, world!".to_string()));
+    println!("{}",length_of_longest_substring("abcabcbb".to_string()));
 }
 pub fn length_of_longest_substring(s: String) -> i32 {
-        s.len() as i32
+    let mut max_len = 1;
+    let mut current_max = 1;
+    let s: Vec<char> = s.chars().collect();
+    let mut i = 0;
+
+    while i < s.len() - 1 {
+        if s[i] == s[i+1] {
+            println!("{}={}",s[i],s[i+1]);
+            current_max = 1;
+        }
+        else {
+            println!("{}!{}",s[i],s[i+1]);
+            current_max +=1;
+        }
+        if max_len < current_max {
+            max_len = current_max; 
+        }
+        i += 1;
     }
+    return max_len as i32;
+}
